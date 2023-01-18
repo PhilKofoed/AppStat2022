90c0b37083e38f69055144a38f5f69db42f15175
PhilKofoed <philip_kofoeddjursner@hotmail.com>
2023-01-18 22:39:08 +0100

    Exam2018

--- Extra_External_Functions/EEF.py
+++ Extra_External_Functions/EEF.py
@@ -71,11 +71,29 @@ def add_text_to_ax(x_coord, y_coord, string, ax, fontsize=12, color='k', halignm
             transform=ax.transAxes, va=valignment, ha = halignment, color=color)
     return None
 
+def simpson38(f, edges, bw, *arg):
+    
+    yedges = f(edges, *arg)
+    left38 = f((2.*edges[1:]+edges[:-1]) / 3., *arg)
+    right38 = f((edges[1:]+2.*edges[:-1]) / 3., *arg)
+    return bw / 8.*( np.sum(yedges)*2.+np.sum(left38+right38)*3. - (yedges[0]+yedges[-1]) ) #simpson3/8
+
+
+def integrate1d(f, bound, nint, *arg):
+    """
+    compute 1d integral
+    """
+    edges = np.linspace(bound[0], bound[1], nint+1)
+    bw = edges[1] - edges[0]
+    
+    return simpson38(f, edges, bw, *arg)
+
 class Datahandler():
-    def __init__(self, data, bins = 10, sy = None, binneddata = True):
+    def __init__(self, data, bins = 10, sy = None, binneddata = True, badvalue=-100000):
         self.data = data
         self.bins = bins
         self.binned = binneddata
+        self.badvalue = badvalue
         if binneddata == True:
             self.xrange = (min(data), max(data))
             y, edges = np.histogram(data, bins, range = self.xrange)
@@ -83,6 +101,7 @@ class Datahandler():
             mask = y > 0
             self.y = y[mask]
             self.x = x[mask]
+            self.binwidth = (self.xrange[1] - self.xrange[0])/self.bins
             if sy == None:
                 sy_poi = np.sqrt(y)
                 self.sy = sy_poi[mask]
@@ -93,7 +112,11 @@ class Datahandler():
             self.y = np.array(data[1])
             self.sy = np.array(data[2])
             self.xrange = (min(self.x), max(self.x))
-        self.lastfittype = None
+            self.binwidth = 1
+        
+
+        self.lastfittypes = None
+        self.nfits = 0
         self.plotinit = False
         
     def basicinfo(self):
@@ -105,10 +128,10 @@ class Datahandler():
             print(f"Data has {entries:d} entries with the mean = {mean:.3f} +/- {eom:.3f} and std = {std:.3f}")
             return entries, mean, std, eom
 
-    def chi2fit(self, func, fulldata = False, **kwargs):
+    def chi2fit(self, fitfunc, fulldata = False, **kwargs):
 
         def obt(*args):
-            yvalues = func(self.x, *args)
+            yvalues = fitfunc(self.x, *args)
             return np.sum((self.y - yvalues)**2/self.sy**2)
 
         chi2_Min = Minuit(obt, **kwargs, name = [*kwargs])
@@ -117,13 +140,14 @@ class Datahandler():
         chi2 = chi2_Min.fval
         Ndof = len(self.x) - chi2_Min.nfit
         prob = stats.chi2.sf(chi2, Ndof)
-        self.fitfunc = func
+        self.fitfunc = fitfunc
         self.chi2valuefit = chi2_Min.fval
         self.Ndoffit = Ndof
         self.probfit = prob
         self.valuesfit = np.array(chi2_Min.values, dtype = np.float64)
         self.errorsfit = np.array(chi2_Min.errors, dtype = np.float64)
         self.lastfittype = "Chi2"
+        self.nfits += 1
         self.fitparams = [*kwargs]
         if not chi2_Min.valid:
             print("!!! Fit did not converge !!!\n!!! Give better initial parameters !!!")
@@ -132,9 +156,48 @@ class Datahandler():
         elif not fulldata:
             return self.valuesfit, self.errorsfit
 
-    def initplot(self, size = (10,7)):
+    def ullhfit(self, fitfunc, extended = False, extended_nint = 100, **kwargs):
+
+        def obt(*args):
+            # !!! Ripped directly from Troels Petersen's work !!!
+            logf = np.zeros_like(self.data)
+            
+            # compute the function value
+            f = fitfunc(self.data, *args)
+        
+            # find where the PDF is 0 or negative (unphysical)        
+            mask_f_positive = f > 0
+
+            # calculate the log of f everyhere where f is positive
+            logf[mask_f_positive] = np.log(f[mask_f_positive])
+            # set everywhere else to badvalue
+            logf[~mask_f_positive] = self.badvalue
+            
+            # compute the sum of the log values: the LLH
+            llh = -np.sum(logf)
+            if extended:
+                extended_term = integrate1d(fitfunc, self.xrange, extended_nint, *args)
+                llh += extended_term
+            return llh
+
+        ullh_Min = Minuit(obt, **kwargs, name = [*kwargs])
+        ullh_Min.errordef = 0.5
+        ullh_Min.migrad()
+        self.fitfunc = fitfunc
+        self.valuesfit = np.array(ullh_Min.values, dtype = np.float64)
+        self.errorsfit = np.array(ullh_Min.errors, dtype = np.float64)
+        self.lastfittype = "ullh"
+        self.nfits += 1
+        self.fitparams = [*kwargs]
+        if not ullh_Min.valid:
+            print("!!! Fit did not converge !!!\n!!! Give better initial parameters !!!")
+        # *** Impliment p-value for ullh fit
+        return self.valuesfit, self.errorsfit
+
+    
+    def initplot(self, figsize = (10,7)):
         # Only singleplot functionallity
-        self.fig, self.ax = plt.subplots(1,1, figsize = size)
+        self.fig, self.ax = plt.subplots(1,1, figsize = figsize)
         self.plotinit = True
             
     def quickplot(self):
@@ -148,25 +211,45 @@ class Datahandler():
         else:
             raise ValueError("No plot initialized. Do initplot()")
 
-    def plotfit(self, xpos = 0.02, ypos = 0.98, extra_spacing=5, decimals=3, N_plotpoints = 10000, **kwargs):
+    def plotfit(self, xpos = 0.02, ypos = 0.98, extra_spacing=-3, decimals=3, N_plotpoints = 10000, textboxspace = 1/3, **kwargs):
         if self.plotinit:
             if self.lastfittype == None:
                 raise ValueError("!!! No fit has been done !!!\nPlease call a fit function before plotting")
             if self.lastfittype == "Chi2":
                 xvalues = np.linspace(*self.xrange, N_plotpoints)
                 yvalues = self.fitfunc(xvalues, *self.valuesfit)
-                plt.plot(xvalues, yvalues, color = "r")
+                self.ax.plot(xvalues, yvalues, color = "r", label = "Chi2 fit")
+                d_title = {"Chi2 fit": ""}
                 d_params = {name: [value, error] for name, value, error in zip(self.fitparams, self.valuesfit, self.errorsfit)}
                 d_rest = {
                     "Chi2": self.chi2valuefit,
                     "Ndof": self.Ndoffit,
                     "Prob": self.probfit
                 }
-                d = {**d_params, **d_rest}
+                d = {**d_title, **d_params, **d_rest}
                 text = nice_string_output(d, extra_spacing=extra_spacing, decimals=decimals)
-                add_text_to_ax(xpos, ypos, text, self.ax, **kwargs)
+                add_text_to_ax(xpos + textboxspace * (self.nfits-1), ypos, text, self.ax, **kwargs)
+                ymin, ymax = self.ax.get_ylim()
+                ymax = ymax + (ymax-ymin)*0.3
+                self.ax.set_ylim(ymin, ymax)
+            if self.lastfittype == "ullh":
+                xvalues = np.linspace(*self.xrange, N_plotpoints)
+                yvalues = self.fitfunc(xvalues, *self.valuesfit) * self.binwidth
+                self.ax.plot(xvalues, yvalues, color = "g", label = "ULLH fit")
+                d_title = {"ULLH fit": ""}
+                d_params = {name: [value, error] for name, value, error in zip(self.fitparams, self.valuesfit, self.errorsfit)}
+                d = {**d_title, **d_params}
+                text = nice_string_output(d, extra_spacing=extra_spacing, decimals=decimals)
+                add_text_to_ax(xpos + textboxspace * (self.nfits-1), ypos, text, self.ax, **kwargs)
                 ymin, ymax = self.ax.get_ylim()
                 ymax = ymax + (ymax-ymin)*0.3
                 self.ax.set_ylim(ymin, ymax)
         else:
             raise ValueError("No plot initialized. Do initplot()")
+
+    def legend(self):
+        self.ax.legend(frameon = False, fontsize = 8, loc = "upper right")
+
+    def savefig(self, name = "Default", dpi = 400):
+        self.fig.tight_layout()
+        self.fig.savefig(name, dpi = dpi)
